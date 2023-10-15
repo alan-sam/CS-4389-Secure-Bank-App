175fc7ee40353ec9ec4bd8302271cb154d33f5e4
Alan Sam <alansambaby@gmail.com>
2023-10-15 18:37:53 -0500

    Login.js and app.cs change

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import '../App.css';
 import { login } from '../api';
 import UTDImage from '../UTD.png';
-import { Button } from '@mui/material';
+import { Button, TextField, Container, Typography, Box } from '@mui/material';
 
 
 function Login() {
@@ -35,34 +35,50 @@ function Login() {
   };
 
   return (
-    <div className='login'>
-        <form>
-        {/* <div className='img'>
-          <img src={UTDImage} alt="UTD Logo" />
-        </div> */}
-            <h3>Login</h3>
-            <div className='mb-2'>
-                <label htmlFor='email'>Email</label>
-                <input type='email' name='email' value={formData.email} onChange={handleInputChange} placeholder='Enter your email address' className='form-control' required/>
-            </div>
-            <div className='mb-2'>  
-            <label htmlFor='password'>Password</label>
-            <input type='password' name='password' value={formData.password} onChange={handleInputChange} placeholder='Enter your password' className='form-control' required/>
-            </div>
-            <div className='d-grid'>
-                   
-                    <Button variant="contained">Contained</Button>
-                    
-            </div>
-            <div className='d-grid'>
-                <Link to = "/dashboard" className='btn btn-primary' >new</Link>
-            </div>
-            <div className='signlink'>
-            <p>
-              Don't have an account? <Link to="/signin"><span>Sign Up</span></Link>
-            </p>
-            </div>
-        </form>
+    <div className="login">
+      <Container component="main" maxWidth="xs">
+        <div className="login-form">
+          <form noValidate onSubmit={handleSubmit}>
+            <Typography component="h1" variant="h5">
+              Login
+            </Typography>
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email"
+              name="email"
+              value={formData.email}
+              onChange={handleInputChange}
+            />
+            <TextField
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              value={formData.password}
+              onChange={handleInputChange}
+            />
+            <Button type="submit" fullWidth variant="contained">
+              Login
+            </Button>
+            <Link to="/dashboard" className="login-link">
+              New
+            </Link>
+            <Box className="login-link">
+              <p>
+                Don't have an account? <Link to="/signin">Sign Up</Link>
+              </p>
+            </Box>
+          </form>
+        </div>
+      </Container>
     </div>
   );
 }
